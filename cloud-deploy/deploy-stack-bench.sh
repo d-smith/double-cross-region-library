8bc5a455403f6d396400a0bb2f3cc3af8943dbf0
d-smith <doug.smith.mail@gmail.com>
2017-10-10 14:05:24 -0700

    Added cloud deploy to run on an ECS cluster with autolaunch/scale for HA

---
+++ cloud-deploy/deploy-stack-bench.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws cloudformation create-stack --stack-name replicator2 \
+--template-body file://replicator.yml --parameters \
+ParameterKey=BucketRoot,ParameterValue=$BUCKET_ROOT \
+ParameterKey=KeyName,ParameterValue=FidoKeyPair \
+ParameterKey=SourceTable,ParameterValue=PKTestTable \
+ParameterKey=DestTable,ParameterValue=PKTestTable \
+ParameterKey=HashAttrName,ParameterValue=id \
+ParameterKey=ClusterName,ParameterValue=r2cluster \
+ParameterKey=InstanceType,ParameterValue=m4.large \
+--capabilities CAPABILITY_IAM
---
+++ cloud-deploy/deploy-stack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+aws cloudformation create-stack --stack-name replicator \
+--template-body file://replicator.yml --parameters \
+ParameterKey=BucketRoot,ParameterValue=$BUCKET_ROOT \
+ParameterKey=KeyName,ParameterValue=FidoKeyPair \
+ParameterKey=SourceTable,ParameterValue=serverless-rest-api-with-dynamodb-hc1 \
+ParameterKey=DestTable,ParameterValue=serverless-rest-api-with-dynamodb-hc1 \
+ParameterKey=HashAttrName,ParameterValue=id \
+ParameterKey=ClusterName,ParameterValue=CharlieFoxtrot \
+--capabilities CAPABILITY_IAM
